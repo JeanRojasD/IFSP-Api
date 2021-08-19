35380be04d508841cc5af0f951354868ca45ffc1
JeanRojasD <71621140+JeanRojasD@users.noreply.github.com>
2021-08-19 16:04:50 -0300

    projeto inicial

---
+++ src/main/java/com/br/ifspapi/IfspApiApplication.java
@@ -0,0 +1,13 @@
+package com.br.ifspapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IfspApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IfspApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/ifspapi/IfspApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.ifspapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IfspApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
