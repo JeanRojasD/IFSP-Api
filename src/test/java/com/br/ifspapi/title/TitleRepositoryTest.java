5ed88736cb5a26eb3801a28210f49e3b66722031
JeanRojasD <71621140+JeanRojasD@users.noreply.github.com>
2021-08-24 20:57:26 -0300

    ajuste no teste de update

--- src/test/java/com/br/ifspapi/title/TitleRepositoryTest.java
+++ src/test/java/com/br/ifspapi/title/TitleRepositoryTest.java
@@ -5,7 +5,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.ArrayList;
